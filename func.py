6b87efd000c1b740f83ab3e67c3a05a6bea74858
Yekong <yekongyt@gmail.com>
2023-04-29 22:41:34 +0800

    Add log handler

--- func.py
+++ func.py
@@ -2,6 +2,8 @@ import youtube_dl
 import discord
 import asyncio
 import requests
+import colorama
+import datetime
 from requests import get
 from environs import Env
 
@@ -98,3 +100,23 @@ def detect_url(message: str):
             return True
         else:
             return False
+        
+
+class LogCommand():
+
+    def __init__(self) -> None:
+        super(LogCommand, self).__init__()
+        # D/M/Y H:M:S
+        self.current_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    def info(self, author: str, msg: str) -> None:
+        msg = f"{colorama.Fore.BLUE}{self.current_time} {colorama.Fore.LIGHTBLUE_EX}{author}{colorama.Fore.RESET} > {colorama.Fore.LIGHTGREEN_EX}{msg}{colorama.Fore.RESET}"
+        print(msg)
+    
+    def error(self, author: str, msg: str) -> None:
+        msg = f"{colorama.Fore.BLUE}{self.current_time} {colorama.Fore.LIGHTBLUE_EX}{author}{colorama.Fore.RESET} > {colorama.Fore.LIGHTRED_EX}{msg}{colorama.Fore.RESET}"
+        print(msg)
+    
+    def warn(self, author: str, msg: str) -> None:
+        msg = f"{colorama.Fore.BLUE}{self.current_time} {colorama.Fore.LIGHTBLUE_EX}{author}{colorama.Fore.RESET} > {colorama.Fore.YELLOW}{msg}{colorama.Fore.RESET}"
+        print(msg)
