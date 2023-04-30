b8bea4a62ddec4e338a1aa6bd9f5f06abc69600e
Yekong <yekongyt@gmail.com>
2023-05-01 00:20:04 +0800

    yt fucntion

---
+++ core/function/yt.py
@@ -0,0 +1,65 @@
+"""
+Youtube downloader for the bot
+"""
+import youtube_dl
+import discord
+import asyncio
+from requests import get
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    # bind to ipv4 since ipv6 addresses cause issues sometimes
+    'source_address': '0.0.0.0',
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
+def search(query):
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        try:
+            get(query)
+        except:
+            search_ = ydl.extract_info(f"ytsearch:{query}", download=False)[
+                'entries'][0]
+        else:
+            search_ = ydl.extract_info(query, download=False)
+
+    return search_
