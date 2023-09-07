bedb1537c8ca1fbcc05680eaa3f780d44cf5aa6f
Yekong995 <yekongyt@gmail.com>
2023-09-07 19:45:21 +0800

    Add skip pytest

--- func.py
+++ func.py
@@ -1,11 +1,34 @@
 import requests
 from environs import Env
 
+# pytest using
+try:
+    import pytest
+except ImportError:
+    pass
 
+def require_env(envs: bool):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if envs is True:
+                return pytest.mark.skip(reason="Environment variable not set while testing")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@require_env(True)
 def get_token() -> str:
     env = Env()
     env.read_env()
     return env.str("DISCORD_TOKEN")
+
+@require_env(True)
+def is_google_api_key_set():
+    google_api_key = Env()
+    google_api_key.read_env()
+    google_api_key = google_api_key.str("GOOGLE_SAFE_BROWSING_API_KEY")
+    is_google_api_key_set = False if google_api_key == "" else True
+    return is_google_api_key_set, google_api_key
 
 
 def detect_url(message: str, apikey: str = None):
