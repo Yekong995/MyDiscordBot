823dc9154511ed8b91e4d20525948d363814fd75
Yekong995 <yekongyt@gmail.com>
2023-09-07 18:27:16 +0800

    utility

---
+++ core/function/utility.py
@@ -0,0 +1,19 @@
+"""
+Define some utility functions.
+"""
+import os
+
+def which(exe_name: str) -> bool:
+    
+    ok = False
+    ok = os.path.isfile(exe_name) and os.access(exe_name, os.X_OK)
+    if ok:
+        return True
+    fpath = os.environ["PATH"].split(os.pathsep)
+    for path in fpath:
+        exe_file = os.path.join(path, exe_name)
+        print(exe_file)
+        if os.path.isfile(exe_file) and os.access(exe_file, os.X_OK):
+            return True
+    return False
+
